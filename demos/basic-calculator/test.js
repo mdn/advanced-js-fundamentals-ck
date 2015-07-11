6897a2ad28df3a64916b8a51912b98c66bf9100b
Steve Kinney <hello@stevekinney.net>
2015-07-11 11:42:23 -0600

    Split up OO section, add more tests

--- demos/basic-calculator/test.js
+++ demos/basic-calculator/test.js
@@ -9,37 +9,107 @@ describe('Calculator', function () {
     assert.equal(calculator.value, 42);
   });
 
-  describe('.add', function () {
-    it('has an add method', function () {
-      var calculator = new Calculator(42);
-      assert(calculator.add);
+  describe('Basic API', function () {
+
+    describe('.add', function () {
+
+      it('has an add method', function () {
+        var calculator = new Calculator(42);
+        assert(calculator.add);
+      });
+
+      it.skip('adds values', function () {
+        var calculator = new Calculator(4);
+        assert(calculator.add(2), 6);
+      });
+
     });
 
-    it('can add to the internal value', function () {
-      var calculator = new Calculator(2);
-      calculator.add(2);
-      assert.equal(calculator.value, 4);
+    describe('.subtract', function () {
+
+      it.skip('has an subtract method', function () {
+        var calculator = new Calculator(42);
+        assert(calculator.subtract);
+      });
+
+      it.skip('subtracts values', function () {
+        var calculator = new Calculator(4);
+        assert(calculator.subtract(2), 2);
+      });
+
     });
+
+    describe('.multiplyBy', function () {
+
+      it.skip('has an multiplyBy method', function () {
+        var calculator = new Calculator(42);
+        assert(calculator.multiplyBy);
+      });
+
+      it.skip('multiplies values', function () {
+        var calculator = new Calculator(4);
+        assert(calculator.multiplyBy(2), 8);
+      });
+
+    });
+
+    describe('.divideBy', function () {
+
+      it.skip('has an divideBy method', function () {
+        var calculator = new Calculator(42);
+        assert(calculator.divideBy);
+      });
+
+      it.skip('divides values', function () {
+        var calculator = new Calculator(4);
+        assert(calculator.divideBy(2), 2);
+      });
+
+    });
+
   });
 
-  describe('.subtract', function () {
-    it.skip('has an add method', function () {
-      var calculator = new Calculator(42);
-      assert(calculator.subtract);
+  describe('Chainable API', function () {
+
+    describe('.add', function () {
+
+      it.skip('can add to the internal value', function () {
+        var calculator = new Calculator(2);
+        calculator.add(2);
+        assert.equal(calculator.value, 4);
+      });
     });
 
-    it.skip('can subtract from the internal value', function () {
-      var calculator = new Calculator(4);
-      assert.equal(calculator.subract(2).value, 2);
+    describe('.subtract', function () {
+      it.skip('has an add method', function () {
+        var calculator = new Calculator(42);
+        assert(calculator.subtract);
+      });
+
+      it.skip('can subtract from the internal value', function () {
+        var calculator = new Calculator(4);
+        assert.equal(calculator.subract(2).value, 2);
+      });
     });
-  });
 
-  describe('.multiply', function () {
-    // Implement your tests here.
-  });
+    describe('.multiply', function () {
+      // Implement your tests here.
+    });
 
-  describe('.divide', function () {
-    // Implement your tests here.
+    describe('.divide', function () {
+      // Implement your tests here.
+    });
+
+    describe('chaining', function () {
+
+      it.skip('can perform multiple operations', function () {
+        var calculator = new Calculator(4);
+        calculator.add(2).subtract(2).multiplyBy(2).divideBy(2);
+        assert.equal(calculator.value, 4);
+      });
+
+    });
+
   });
 
 });
