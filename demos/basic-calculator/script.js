a1cbc2428ddf92d552f147871b73919836d2e83b
mrjaimisra <misrajai01@gmail.com>
2015-10-21 11:26:17 -0600

    Make some tests pass in the calculator and canvas tutorials

--- demos/basic-calculator/script.js
+++ demos/basic-calculator/script.js
@@ -5,4 +5,20 @@ function Calculator(value) {
 Calculator.prototype.add = function (addend) {
   this.value += addend;
   return this;
-}
+};
+
+Calculator.prototype.subtract = function (subtrahend) {
+  this.value -= subtrahend;
+  return this;
+};
+
+Calculator.prototype.multiplyBy = function (factor) {
+  this.value *= factor;
+  return this;
+};
+
+Calculator.prototype.divideBy = function (divisor) {
+  this.value /= divisor;
+  return this;
+};
+
