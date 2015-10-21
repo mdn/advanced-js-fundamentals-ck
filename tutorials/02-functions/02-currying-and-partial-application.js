cd497a629173e1976492daa59395f3fd01e82dfb
Austin Wood <i@austinwood.me>
2015-10-21 16:01:12 -0600

    Create tests for currying

--- tutorials/02-functions/02-currying-and-partial-application.js
+++ tutorials/02-functions/02-currying-and-partial-application.js
@@ -6,10 +6,24 @@ function multiply(a, b) {
   return a * b;
 }
 
+// Use partial application to finish the following:
+
 var addOne;
 var subtractThree;
 var doubleNumber;
 var halveNumber;
+
+// Create a function called `prefixLog` that takes a prefix as an argument and uses
+// partial application and/or currying to return a function that takes a message and
+// adds the prefix to the beginning of the message.
+
+function prefixLog() {}
+
+// Implement a `dangerLog` function that takes a message and prefixes it with `DANGER:`.
+// Implement a `successLog` function that takes a message and prefixes it with `SUCCESS:`.
+
+var dangerLog;
+var successLog;
 
 if (typeof window === 'undefined') {
   module.exports = {
@@ -17,5 +31,7 @@ if (typeof window === 'undefined') {
     subtractThree: subtractThree,
     doubleNumber: doubleNumber,
     halveNumber: halveNumber,
+    dangerLog: dangerLog,
+    successLog: successLog,
   };
 }
--- tutorials/02-functions/test/02-currying-and-partial-application-test.js
+++ tutorials/02-functions/test/02-currying-and-partial-application-test.js
@@ -5,10 +5,15 @@ if (typeof window === 'undefined') {
   var subtractThree = Partials.subtractThree;
   var doubleNumber = Partials.doubleNumber;
   var halveNumber = Partials.halveNumber;
+
+  var Currying = require('../02-currying-and-partial-application.js');
+  var prefixLog = Currying.prefixLog;
+  var dangerLog = Currying.dangerLog;
+  var successLog = Currying.successLog;
 }
 
 describe('02 - Functions', function () {
-  describe('Partials', function () {
+  describe('Partial Application', function () {
     it.skip('has a function that adds 1 to a number', function () {
       expect(addOne(5)).to.equal(6);
     });
@@ -23,6 +28,31 @@ describe('02 - Functions', function () {
 
     it.skip('has a function that halves a number', function () {
       expect(halveNumber(5)).to.equal(2.5);
+    });
+  });
+
+  describe('Currying', function () {
+    describe('prefixLog', function () {
+      it.skip('should return a function', function () {
+        expect(typeof prefixLog('PREFIX')).to.equal('function');
+      });
+
+      it.skip('should return a function that accepts a message and uses the prefix', function () {
+        var prefixWithPrefix = prefixLog('PREFIX');
+        expect(prefixWithPrefix('message')).to.equal('PREFIX: message');
+      });
+    });
+
+    describe('dangerLog', function () {
+      it.skip('should prefix a message with DANGER:', function () {
+        expect(dangerLog('Oh no')).to.equal('DANGER: Oh no');
+      });
+    });
+
+    describe('successLog', function () {
+      it.skip('should prefix a message with SUCCESS:', function () {
+        expect(successLog('Oh no')).to.equal('SUCCESS: Oh no');
+      });
     });
   });
 });
